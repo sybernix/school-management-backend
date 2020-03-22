47ffbf1903a2ae30548bfe29f0a22197f6d86f03
niruhan <niruhanv@gmail.com>
2020-03-22 20:47:18 +0530

    Add parents schema

---
+++ schemas/parent_schema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const configs = require("../config/config.json");
+
+const schema = new mongoose.Schema({
+    parentID: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+});
+
+const parentSchema = mongoose.model(configs.PARENT_COLLECTION_NAME, schema);
+module.exports = parentSchema;
