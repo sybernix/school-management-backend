3aa58ba12326052ff2119ae645f7252923368cb6
niruhan <niruhanv@gmail.com>
2020-04-11 11:29:08 +0530

    Add route to get list of homeworks

--- routes/homework_routes.js
+++ routes/homework_routes.js
@@ -4,12 +4,13 @@ const configs = require("../config/config.json");
 const constants = require("../utils/constants");
 const utils = require("../utils/extract_token");
 const fs = require('fs-extra');       //File System - for file manipulation
+// const json = require('json-simple');
 
 const router = express.Router();
 
 //Upload homework route
 router.post("/upload", utils.extractToken, (req, res) => {
-    jwt.verify(req.token, configs.JWT_KEY_ADMIN, (err, authData) => {
+    jwt.verify(req.token, configs.JWT_KEY_TEACHER, (err, authData) => {
         if(err) {
             res.sendStatus(403);
         } else {
@@ -32,12 +33,30 @@ router.post("/upload", utils.extractToken, (req, res) => {
 });
 
 router.get("/download/:file(*)", utils.extractToken, (req, res) => {
-    jwt.verify(req.token, configs.JWT_KEY_ADMIN, (err, authData) => {
+    jwt.verify(req.token, configs.JWT_KEY_TEACHER, (err, authData) => {
         if(err) {
             res.sendStatus(403);
         } else {
             const file = req.params.file;
             res.download(constants.HOMEWORK_DIRECTORY_PATH + file);
+        }
+    });
+});
+
+router.get("/getList", utils.extractToken, (req, res) => {
+    jwt.verify(req.token, configs.JWT_KEY_TEACHER, (err, authData) => {
+        if(err) {
+            res.sendStatus(403);
+        } else {
+            var fileJSON = {};
+            var i =1;
+            fs.readdir(constants.HOMEWORK_DIRECTORY_PATH, (err, files) => {
+                files.forEach(file => {
+                    fileJSON[i] = file;
+                    i = i + 1;
+                });
+                return res.status(200).json(fileJSON);
+            });
         }
     });
 });
