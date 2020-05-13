0e03a4ac8a0f49be7041da030d30854fa5e3b107
Lankitha Gallage <lankitha.mailbox@gmail.com>
2020-05-13 18:46:25 +0530

    Release Commit

--- schemas/m/class_schema.js
+++ schemas/m/class_schema.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 const constants = require("../../utils/constants");
 
 const schema = new mongoose.Schema({
-    class_name: {
-        type: String
-    },
-    class_section_id: {
-        type: String
-    },
-    is_active: {
-        type: Boolean
-    }
+  class_name: {
+    type: String,
+  },
+  class_section_id: {
+    type: String,
+  },
+  is_active: {
+    type: Boolean,
+  },
 });
 
-const compiledSchema = mongoose.model(constants.M_CLASS_COLLECTION_NAME, schema);
+const compiledSchema = mongoose.model(
+  constants.M_CLASS_COLLECTION_NAME,
+  schema
+);
 module.exports = compiledSchema;
