49eec1e59257e911c50788930f05dfb511a93fc8
niruhan <niruhanv@gmail.com>
2020-05-14 21:45:06 +0530

    Fix joined user socket

--- sockets/joinedUser.js
+++ sockets/joinedUser.js
@@ -1,5 +1,6 @@
 const socketEvents = require("../utils/socket_events");
 const MongoClient = require('mongodb').MongoClient;
+const chatMessageSchema = require("../schemas/chat_message_schema");
 const configs = require("../config/config.json");
 
 module.exports = (io, socket) => {
@@ -8,17 +9,29 @@ module.exports = (io, socket) => {
 		console.log("Client Socket ID : " + socket.id);
 		console.log(user);
 
-		MongoClient.connect(configs.MONGO_URI, function(err, db) {
-			if (err) throw err;
-			const dbo = db.db(configs.MONGO_DB_NAME);
-			const query = {receiver: user.userId};
-			dbo.collection(configs.CHAT_MESSAGES_COLLECTION_NAME).find(query).toArray(function(err, result) {
-				if (err) throw err;
-				socket.emit("received messages", result);
-				console.log(result);
-				// io.in(socket.id).emit("received messages", result);
-				db.close();
+		const query = {receiver: user.userId};
+		chatMessageSchema
+			.find(query)
+			.exec()
+			.then((resultList) => {
+				if (resultList) {
+					socket.emit("received messages", resultList);
+					console.log(resultList);
+				}
 			});
-		});
+		// chatMessageSchema.find(query).toArray(function(err, result) {
+		// 	if (err) throw err;
+		// 	socket.emit("received messages", result);
+		// 	console.log(result);
+		// 	// io.in(socket.id).emit("received messages", result);
+		// 	db.close();
+		// });
+
+		// MongoClient.connect(configs.MONGO_URI, function(err, db) {
+		// 	if (err) throw err;
+		// 	// const dbo = db.db(configs.MONGO_DB_NAME);
+		//
+		//
+		// });
 	});
 };
