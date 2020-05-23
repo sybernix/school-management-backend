4637b637b574ff5340d6dd54a7d4650dbccc951e
niruhan <niruhanv@gmail.com>
2020-05-23 19:24:56 +0530

    Upload homework in class and subject sub dirs

--- routes/homework_routes.js
+++ routes/homework_routes.js
@@ -5,6 +5,7 @@ const constants = require("../utils/constants");
 const utils = require("../utils/util_methods");
 const fs = require('fs-extra');
 const tokenSchema = require("../schemas/token_schema");
+const fs_for_mkdir = require('fs');
 
 const router = express.Router();
 
@@ -19,12 +20,23 @@ router.post("/upload", utils.extractToken, (req, res) => {
                     message: "Invalid Token",
                 });
             }
+            console.log(req.headers['class_id']);
+            const class_id = req.headers['class_id'];
+            const subject_id = req.headers['subject_id'];
+            const upload_path = constants.HOMEWORK_DIRECTORY_PATH + class_id + "/" + subject_id;
+            if (!fs_for_mkdir.existsSync(constants.HOMEWORK_DIRECTORY_PATH + class_id)){
+                fs_for_mkdir.mkdirSync(constants.HOMEWORK_DIRECTORY_PATH + class_id);
+            }
+            if (!fs_for_mkdir.existsSync(upload_path)){
+                fs_for_mkdir.mkdirSync(upload_path);
+            }
+
             let fileStream;
             req.pipe(req.busboy);
             req.busboy.on('file', function (fieldName, file, fileName) {
                 console.log("Uploading: " + fileName);
                 //Path where homework will be uploaded
-                fileStream = fs.createWriteStream(constants.HOMEWORK_DIRECTORY_PATH + fileName);
+                fileStream = fs.createWriteStream(upload_path + "/" +  fileName);
                 file.pipe(fileStream);
                 fileStream.on('close', function () {
                     console.log("Upload Finished of " + fileName);
